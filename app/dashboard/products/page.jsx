8f4cc642e14fd74c723f1f52c59467ddc73e928c
DevAiKaiSys <dev.aikaisys@gmail.com>
2023-11-11 21:15:28 +0700

    Login Page Design

--- app/dashboard/products/page.jsx
+++ app/dashboard/products/page.jsx
@@ -50,7 +50,7 @@ const ProductsPage = async ({ searchParams }) => {
             <td>72</td>
             <td>
               <div className={styles.buttons}>
-                <Link href="/">
+                <Link href="/dashboard/products/test">
                   <button className={`${styles.button} ${styles.view}`}>
                     View
                   </button>
