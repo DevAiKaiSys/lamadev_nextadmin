67536973156cfbd7f070cc4dd52d23d7039df67a
DevAiKaiSys <dev.aikaisys@gmail.com>
2023-11-12 14:00:54 +0700

    Next.js 14 Update Item with Server Actions

--- app/dashboard/users/[id]/page.jsx
+++ app/dashboard/users/[id]/page.jsx
@@ -1,4 +1,4 @@
-// import { updateUser } from "@/app/lib/actions";
+import { updateUser } from '@/app/lib/actions';
 import { fetchUser } from '@/app/lib/data';
 import styles from '@/app/ui/dashboard/users/singleUser/singleUser.module.css';
 import Image from 'next/image';
@@ -41,8 +41,8 @@ const SingleUserPage = async ({ params }) => {
           </select>
           <button>Update</button>
         </form> */}
-        <form action="" className={styles.form}>
-          {/* <form action={updateUser} className={styles.form}> */}
+        {/* <form action="" className={styles.form}> */}
+        <form action={updateUser} className={styles.form}>
           <input type="hidden" name="id" value={user.id} />
           <label>Username</label>
           <input type="text" name="username" placeholder={user.username} />
@@ -55,22 +55,14 @@ const SingleUserPage = async ({ params }) => {
           <label>Address</label>
           <textarea type="text" name="address" placeholder={user.address} />
           <label>Is Admin?</label>
-          <select name="isAdmin" id="isAdmin">
-            <option value={true} selected={user.isAdmin}>
-              Yes
-            </option>
-            <option value={false} selected={!user.isAdmin}>
-              No
-            </option>
+          <select name="isAdmin" id="isAdmin" defaultValue={user.isAdmin}>
+            <option value={true}>Yes</option>
+            <option value={false}>No</option>
           </select>
           <label>Is Active?</label>
-          <select name="isActive" id="isActive">
-            <option value={true} selected={user.isActive}>
-              Yes
-            </option>
-            <option value={false} selected={!user.isActive}>
-              No
-            </option>
+          <select name="isActive" id="isActive" defaultValue={user.isActive}>
+            <option value={true}>Yes</option>
+            <option value={false}>No</option>
           </select>
           <button>Update</button>
         </form>
