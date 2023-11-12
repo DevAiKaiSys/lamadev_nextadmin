e875f432f27709f8bd55d529d8d745312ee81b19
DevAiKaiSys <dev.aikaisys@gmail.com>
2023-11-12 19:03:29 +0700

    useFormState Hook Tutorial (Next.js Server Actions Error Handling)

--- app/ui/login/loginForm/loginForm.jsx
+++ app/ui/login/loginForm/loginForm.jsx
@@ -5,6 +5,11 @@ import styles from './loginForm.module.css';
 import { useFormState } from 'react-dom';
 
 const LoginForm = () => {
+  // const [err, setErr] = useState();
+  // const handleLogin = async (formData) => {
+  //   const data = await authenticate(formData);
+  //   data.error && setErr(data.error);
+  // };
   const [state, formAction] = useFormState(authenticate, undefined);
 
   return (
@@ -15,6 +20,7 @@ const LoginForm = () => {
       <input type="text" placeholder="username" name="username" />
       <input type="password" placeholder="password" name="password" />
       <button>Login</button>
+      {/* {err && err} */}
       {state && state}
     </form>
   );
